91cd005f696962e0c6a2d50b40ddc9426018c935
nbikhcha <nbikhcha@andrew.cmu.edu>
2013-05-12 09:46:05 -0300

    Update mothersday.js

--- mothersday.js
+++ mothersday.js
@@ -1,5 +1,5 @@
-var $body = $("<body>");
+\
 var $div = $("<div>").attr("id", "1");
-$body.children().append($div);
-$("html").children().append($body);
+$("body").children().append($div);
+$("html").children();
 $("#1").html('Happy Mother\'s Day Mama!');
