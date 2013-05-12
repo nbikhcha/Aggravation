6c8a8139fec35c84a60c229398250bb928928bdd
nbikhcha <nbikhcha@andrew.cmu.edu>
2013-05-12 02:16:53 -0300

    Update mothersday.js

--- mothersday.js
+++ mothersday.js
@@ -1,2 +1,14 @@
-document.write("yay!")
-alert("dunzo");
+
+if (window.XMLHttpRequest)
+{// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+}
+xmlhttp.onreadystatechange=function()
+{
+    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+        alert(xmlhttp.responseText);
+    }
+}
+xmlhttp.open("GET","https://github.com/nbikhcha/Aggravation/blob/master/mothersday.html",true);
+xmlhttp.send();
