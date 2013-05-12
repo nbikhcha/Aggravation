d8fd22b8c376e2b32b908beb351d00a671c127b9
nbikhcha <nbikhcha@andrew.cmu.edu>
2013-05-12 02:02:12 -0300

    Update mothersday.js

--- mothersday.js
+++ mothersday.js
@@ -1,12 +1,2 @@
-window.location.href="https://raw.github.com/nbikhcha/Aggravation/master/mothersday.html";
-html = ""
-window.onload = function(){
-  console.log("loaded");
-  html = $("pre").innerHTML;
-  
-  function unescapeHTML(escapedHTML) {
-    return escapedHTML.replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&amp;/g,'&');
-  } 
-  
-  html = unescapeHTML(html);
-};
+document.write("yay!")
+alert("dunzo");
